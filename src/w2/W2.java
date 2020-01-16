236de3b244045d7d86d280e6f371d491d79c7684
truonngh <truonngh@sheridancollege.ca>
2020-01-16 10:50:29 -0500

    student code

---
+++ src/w2/W2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w2;
+
+/**
+ *
+ * @author Steve
+ */
+public class W2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("learning github");
+    }
+    
+}
